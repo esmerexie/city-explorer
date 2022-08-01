120411215f5207ef23a6efdc3ddf84231a1d6d11
Rexie Esmeralda <esmeraldarexie@gmail.com>
2022-07-31 21:39:41 -0700

    added map intergration

---
+++ src/components/CityModal.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Modal, Button, Container, Image } from 'react-bootstrap';
+
+
+class CityModal extends React.Component {
+  
+
+ 
+  render() {
+    return (
+      <>
+
+        <Modal
+        show={this.props.showModal}
+          onHide={this.props.handleClose}
+          size={"l"}>
+          <Modal.Header closeButton>
+            <Modal.Title>{this.props.location.display_name}</Modal.Title>
+          </Modal.Header>
+          <Modal.Body> The coodinates of : {this.props.location.display_name} is {this.props.location.lat}, {this.props.location.lon}
+          <Container>
+              <Image
+                style={{ width: '400px' }}
+                src={this.props.displayMap} />
+
+            </Container>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="primary" onClick={this.props.handleClose}>
+              Close
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </>
+    );
+  }
+}
+export default CityModal
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,45 +1,49 @@
 import React from 'react';
 import axios from 'axios';
+// import { Modal, Button,} from 'react-bootstrap';
+import CityModal from './CityModal.js';
+
 
 
 class Main extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state={
-      lat: 0,
-      lon: 0,
-      searchQuery:'',
-      weather: [],
+    this.state = {
+      searchQuery: '',
       location: {},
-      map: ''
+      show: false,
+      displayMap: '',
+
     }
   }
 
   getLocation = async () => {
-    console.log(this.state.searchQuery);
     const API = `https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_CITY_KEY}&q=${this.state.searchQuery}&format=json`;
     const res = await axios.get(API);
-    console.log(res.data[0])
-    this.setState({ location:res.data[0] });
+    console.log(res.data)
+    this.setState({
+      location: res.data[0],
+      show: true,
+      displayMap: true
+    });
   }
+  handleClose = () => this.setState({ show: false });
 
 
-  getWeather = () => {
-    let url = `http://localhost:3001?lat=${this.state.lat}&lon=${this.state.lon}&searchQuery=${this.forceUpdate.searchQuery}`;
-  }
-
-  render() {
-    return(
-      <>
-        <input onChange={(e) => this.setState({ searchQuery: e.target.value })} placeholder="search for a city"></input>
-        <button onClick={this.getLocation}>Explore!</button>
-        {this.state.location.place_id &&
-          <h2>The city is: {this.state.location.display_name} Latitude: {this.state.location.lat} Longitude:{this.state.location.lat}</h2>
-          
-        }
+render() {
+  return (
+    <>
+      <input onChange={(e) => this.setState({ searchQuery: e.target.value })} placeholder="search for a city"></input>
+      <button onClick={this.getLocation}>Explore!</button>
+      <CityModal
+        location={this.state.location}
+        handleClose={this.handleClose}
+        showModal={this.state.show}
+        displayMap={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_CITY_KEY}&center=${this.state.location.lat},${this.state.location.lon}&zoom=12`}
+      />
       </>
-    )
-  }
+  )
+}
 }
 
 export default Main;
